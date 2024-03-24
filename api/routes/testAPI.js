06770958e3ed1007de8d756f9b7ba16cfb428006
Mikan <104711459+MikanW@users.noreply.github.com>
2024-03-24 23:14:30 +0900

    add mongoDB connection and sample data displayed on top page

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,8 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var mongoDB = require("./../db.js");
+const { MongoClient } = require("mongodb");
 
-router.get('/', function(req, res, next) {
-    res.send('API is working properly');
+const client = new MongoClient(mongoDB.uri);
+
+async function getPhotos() {
+  var allPhotos = [];
+
+  try {
+    await client.connect();
+    // Get the database and collection on which to run the operation
+    const db = client.db("photos");
+    const col = db.collection("photo");
+
+    // Get the database and collection on which to run the operation
+    const cursor = col.find({});
+
+    // Find the document
+    allPhotos = await cursor.toArray();
+    // Print results
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+  }
+
+  return allPhotos;
+}
+
+router.get('/', async function (req, res, next) {
+  const allPhotos = await getPhotos();
+  res.send(allPhotos);
 });
 
 module.exports = router;
